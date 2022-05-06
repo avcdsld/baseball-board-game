fb768bf5fc39254d15903e96cd70aaf01198b758
0xys <yushi.kinger@gmail.com>
2022-05-06 21:39:35 +0900

    success connecting wallet

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -2,8 +2,19 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
+
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 
 const Home: NextPage = () => {
+  const connectWithMetamask = useMetamask();
+  const disconnect = useDisconnect()
+  const address = useAddress();
+
+  useEffect(() => {
+    console.log('address:', address)
+  }, [address])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,46 +23,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <p>address: {address ?? 'not connected'}</p>
+      <button onClick={() => !address ? connectWithMetamask() : disconnect()}>{ !address ? "Connect Wallet" : "Disconnect"}</button>
 
       <footer className={styles.footer}>
         <a
