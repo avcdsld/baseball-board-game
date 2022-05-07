f7196d4ea51080c892b5716f0fd030de1acd675a
0xys <yushi.kinger@gmail.com>
2022-05-07 15:34:19 +0900

    bigger batting result

--- packages/frontend/src/pages/components/result.tsx
+++ packages/frontend/src/pages/components/result.tsx
@@ -47,14 +47,16 @@ export const ResultComponent = (prop: {
     }
 
     return (
-        <HStack spacing={2}>
-            <Badge variant={varient} colorScheme={colorScheme}>{result}</Badge>
-            <Text fontWeight='bold'>score</Text>
-            <Badge variant='solid' colorScheme='blackAlpha'>{score}</Badge>
-            <Text fontWeight='bold'>by</Text>
-            <Link href={`https://mumbai.polygonscan.com/address/${batter}`} isExternal>
-                {batter.slice(0, 12)}... <ExternalLinkIcon mx='2px'/>
-            </Link>
-        </HStack>
+        <VStack spacing={2}>
+            <Badge variant={varient} colorScheme={colorScheme} fontSize='1.2em'>{result}</Badge>
+            <HStack spacing={2}>
+                <Text fontWeight='bold'>score</Text>
+                <Badge variant='solid' colorScheme='blackAlpha'>{score}</Badge>
+                <Text fontWeight='bold'>by</Text>
+                <Link href={`https://mumbai.polygonscan.com/address/${batter}`} isExternal>
+                    {batter.slice(0, 12)}... <ExternalLinkIcon mx='2px'/>
+                </Link>
+            </HStack>
+        </VStack>
     )
 }
